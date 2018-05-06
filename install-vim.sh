7585a146b2ab88cb885022a064ba5875d4f739d7
Sangjee Dondrub <sangjeedondrub@live.com>
2018-05-06 21:34:22 +0800

    Delete install-vim.sh

--- install-vim.sh
+++
@@ -1,4 +0,0 @@
-#curl http://j.mp/spf13-vim3 -L -o - | sh
-wget https://raw.githubusercontent.com/spf13/spf13-vim/3.0/bootstrap.sh -O /tmp/install-vim.sh
-sudo chmod +x /tmp/install-vim.sh
-/tmp/install-vim.sh
