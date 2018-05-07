1f33742bca96063eba1d08a26bc5992b5913898b
Lhama Tashi <lhamatashi@outlook.com>
2018-05-07 16:49:52 +0800

    shift over to archlinux

--- init.sh
+++ init.sh
@@ -1,32 +1,13 @@
 # INSTALL COMMON TOOLS
 #######################################################
-sudo apt-get update -y && \
-sudo apt-get install -y wget \
-						git \
-						vim \
-						zsh \
-						python-pip \
-						python3-pip \
-						virtualenv
-
-# HOSTS
-#######################################################
-wget https://raw.githubusercontent.com/racaljk/hosts/master/tools/lhosts
-sudo mv lhosts /usr/local/bin/lhosts
-sudo chmod +x /usr/local/bin/lhosts
 
 # Init zsh
 #######################################################
 # Shift to zsh from bash
 sudo chsh -s $(which zsh)
 
-# remove ~/.oh-myzsh folder if exists
-if [-d ~/.oh-my-zsh ];then
-  mv ~/.oh-my-zsh ~/.oh-my-zsh-back
-fi
-
 # Config zsh with oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Init vim
 #######################################################
@@ -34,7 +15,6 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s -f ~/code/dotfiles/.vimrc ~/.vimrc
 vim +PluginInstall +qall
-
 
 
 # LINKING DOTFILES
